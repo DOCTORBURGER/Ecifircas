2760eaaca4daff4069b0a8721065c7a9e47b33ca
Jonah Bogusch <jonahmbog@gmail.com>
2024-01-29 15:10:13 -0600

    Started basic uci functionality

--- Ecifircas/Ecifircas/Ecifircas.cpp
+++ Ecifircas/Ecifircas/Ecifircas.cpp
@@ -2,10 +2,36 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string inputLine;
+    bool running = true;
+
+    while (running) {
+        if (!std::getline(std::cin, inputLine)) {
+            break; 
+        }
+
+        if (inputLine == "uci") {
+            std::cout << "id name Ecifircas" << std::endl;
+            std::cout << "id author Jonah Bogusch" << std::endl;
+
+            std::cout << "uciok" << std::endl;
+        }
+        else if (inputLine == "isready") {
+            std::cout << "readyok" << std::endl;
+        }
+        else if (inputLine == "quit") {
+            running = false;
+        }
+        else {
+            
+        }
+    }
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
