c5589fde2b221196120a215ac9f3c1504baf1550
Jonah Bogusch <jonahmbog@gmail.com>
2024-02-28 19:22:17 -0600

    Added move class

--- Ecifircas/Ecifircas/types.h
+++ Ecifircas/Ecifircas/types.h
@@ -85,9 +85,56 @@ namespace Ecifircas {
 		BLACK_QUEENSIDE = 8
 	};
 
-	// We may need to change this later, either to a short for efficiency, or add more fields for promotion
-	typedef struct {
-		int start;
-		int dest;
-	} move;
+	enum MoveFlags : int
+	{
+		QUIET_MOVE = 0,
+		DOUBLE_PAWN_PUSH = 1,
+		KING_CASTLE = 2,
+		QUEEN_CASTLE = 3,
+		CAPTURE = 4,
+		EP_CAPTURE = 5,
+		KNIGHT_PROMO = 8, // Jump in size here for bits (1000), ints are "codes"
+		BISHOP_PROMO = 9,
+		ROOK_PROMO = 10,
+		QUEEN_PROMO = 11,
+		KNIGHT_PROMO_CAPTURE = 12,
+		BISHOP_PROMO_CAPTURE = 13,
+		ROOK_PROMO_CAPTURE = 14,
+		QUEEN_PROMO_CAPTURE = 15
+	};
+
+	// 0000 0000 0000 0011 1111 source square
+	// 0000 0000 1111 1100 0000 target square
+	// 0000 0111 0000 0000 0000 piece type
+	// 0000 1000 0000 0000 0000 color
+	// 1111 0000 0000 0000 0000 flag
+	// Used this flag structure: https://www.chessprogramming.org/Encoding_Moves
+	// Took operator inspiration from stockfish
+	class Move
+	{
+		Move(Square source, Square dest, Piece piece, Color color, MoveFlags flags)
+		{
+			data = (flags & 0xf) << 16 | (color) << 15 | (piece & 0x3) << 12
+				| (dest & 0x3f) << 6 | (source & 0x3f);
+		}
+
+		constexpr Square from_sq()	const { return Square(data & 0x3f); }
+
+		constexpr Square to_sq()	const { return Square((data >> 6) & 0x3f); }
+
+		constexpr int to_from_sq()	const { return data & 0xfff; }
+
+		constexpr MoveFlags flags() const { return MoveFlags((data >> 16) & 0xf); }
+
+		constexpr Piece get_piece() const { return Piece((data >> 12) & 0x7); }
+
+		constexpr Color get_color() const { return Color(data >> 15 & 0x1); }
+
+		constexpr bool operator==(const Move& m) const { return data == m.data; }
+		constexpr bool operator!=(const Move& m) const { return data != m.data; }
+
+		constexpr explicit operator bool() const { return data != 0; }
+	protected:
+		int data;
+	};
 }
