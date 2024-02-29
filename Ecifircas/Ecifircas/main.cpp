d2b1e7107c4d8d8851814c1deae414fe209b8d89
Jonah Bogusch <jonahmbog@gmail.com>
2024-02-29 04:04:38 -0600

    Switched to favor optimizaiton, PERFT runs at least 3x faster

--- Ecifircas/Ecifircas/main.cpp
+++ Ecifircas/Ecifircas/main.cpp
@@ -15,7 +15,7 @@ int main()
     bool running = true;
     initialize_bitboards();
 
-    set_board("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 1 0"); // "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
+    set_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); // "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
 
     while (running) {
         if (!std::getline(std::cin, inputLine)) {
