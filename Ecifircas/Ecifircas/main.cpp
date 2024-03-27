677583728596a877dfd7443a40d11b00753af7ea
Jonah Bogusch <jonahmbog@gmail.com>
2024-03-27 14:33:26 -0500

    Adding more UCI support

--- Ecifircas/Ecifircas/main.cpp
+++ Ecifircas/Ecifircas/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <random>
 
+#include "types.h"
 #include "board.h"
 #include "bitboard.h"
 #include "movegen.h"
@@ -14,30 +17,78 @@ int main()
     bool running = true;
     initialize_bitboards();
 
-    set_board("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"); // "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
+    set_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
     while (running) {
         if (!std::getline(std::cin, inputLine)) {
             break; 
         }
 
-        if (inputLine == "uci") {
+        std::istringstream iss(inputLine);
+        std::string token, fen;
+        iss >> token;
+
+        if (token == "uci") {
             std::cout << "id name Ecifircas" << std::endl;
             std::cout << "id author Jonah Bogusch" << std::endl;
 
             std::cout << "uciok" << std::endl;
         }
-        else if (inputLine == "isready") {
+        else if (token == "isready") {
             std::cout << "readyok" << std::endl;
         }
-        else if (inputLine == "quit") {
+        else if (token == "quit" || token == "q") {
             running = false;
         }
-        else if (inputLine == "d") {
+        else if (token == "d") {
             print_board();
         }
-        else if (inputLine == "perft") {
-            perft_test(4);
+        else if (token == "position") {
+            iss >> token;
+
+            if (token == "startpos") {
+                set_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+                iss >> token;
+            }
+            else if (token == "fen") {
+                while (iss >> token && token != "moves")
+                    fen += token + " ";
+
+                size_t endpos = fen.find_last_not_of(" \t");
+                if (std::string::npos != endpos) {
+                    fen = fen.substr(0, endpos + 1);
+                }
+
+                set_board(fen);
+            }
+            else {
+
+            }
+
+            if (token == "moves") {
+                while (iss >> token) {
+                    
+                }
+            }
+        }
+        else if (token == "go") {
+            Moves moves;
+            generate_moves(moves);
+            moves.get_move(0);
+            std::string promoString = "nbrq";
+            size_t index = moves.get_move(0).flags() & 0x3;
+            std::string promoChar = (index < promoString.size()) ? std::string(1, promoString[index]) : std::string(1, '\0');
+            std::cout << "bestmove " << SquareToCoordinates[moves.get_move(0).from_sq()] << 
+                SquareToCoordinates[moves.get_move(0).to_sq()] << std::endl;
+        }
+        else if (token == "perft") {
+            int depth;
+            if (iss >> depth) {
+                perft_test(depth);
+            }
+            else {
+                std::cout << "Error: 'perft' command requires a depth argument." << std::endl;
+            }
         }
         else {
 
